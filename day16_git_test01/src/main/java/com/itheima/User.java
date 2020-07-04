f4c074fa4c07b5b9739d9c0d49e57e7c5b408965
Lewis <846615543@qq.com>
2020-07-04 15:34:53 +0800

    第一次提交

---
+++ day16_git_test01/src/main/java/com/itheima/User.java
@@ -0,0 +1,28 @@
+package com.itheima;
+
+/**
+ * <h3>Framework01</h3>
+ * <p></p>
+ *
+ * @author : Lewis
+ * @date : 2020-07-04 14:50
+ **/
+public class User {
+
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
